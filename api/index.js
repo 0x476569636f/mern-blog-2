ea8cb48be74422993da6e4e2284eacc402ff92ff
Irvan Pramana Putra <darknet1255@gmail.com>
2024-06-14 16:09:15 +0700

    Create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoute.js';
+import authRoutes from './routes/authRoute.js';
 
 dotenv.config();
 
@@ -11,9 +12,11 @@ mongoose
 .catch((error) => console.log(error))
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server running !');
 })
 
 app.use('/api/user', userRoutes)
+app.use('/api/auth', authRoutes)
