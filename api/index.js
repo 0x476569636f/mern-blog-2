67c06589f0ecc02c3367dc2cde2611daf5f6b370
Irvan Pramana Putra <darknet1255@gmail.com>
2024-06-14 16:31:22 +0700

    Add: Middleware to handler error

--- api/index.js
+++ api/index.js
@@ -1,22 +1,28 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRoutes from './routes/userRoute.js';
-import authRoutes from './routes/authRoute.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoute.js";
+import authRoutes from "./routes/authRoute.js";
 
 dotenv.config();
 
 mongoose
-.connect(process.env.MONGO)
-.then(() => console.log('MongoDb is Connected'))
-.catch((error) => console.log(error))
+  .connect(process.env.MONGO)
+  .then(() => console.log("MongoDb is Connected"))
+  .catch((error) => console.log(error));
 
 const app = express();
 app.use(express.json());
 
 app.listen(3000, () => {
-    console.log('Server running !');
-})
+  console.log("Server running !");
+});
 
-app.use('/api/user', userRoutes)
-app.use('/api/auth', authRoutes)
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({ success: false, statusCode, message });
+});
